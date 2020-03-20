bf4490a7829a88456d68965e049cb8c5cb6f5662
Pinchuk Yevhen <jagernaut1233@gmail.com>
2020-03-20 18:39:42 +0200

    added Sender

---
+++ src/main/java/pinchuk/sfgjms/sender/HelloSender.java
@@ -0,0 +1,36 @@
+package pinchuk.sfgjms.sender;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import pinchuk.sfgjms.config.JmsConfig;
+import pinchuk.sfgjms.model.HelloWorldMessage;
+
+import java.util.UUID;
+
+/**
+ * @author Pinchuk Yevhen
+ * @created 20/03/2020 - 18:30
+ */
+@RequiredArgsConstructor
+@Component
+public class HelloSender {
+
+    private final JmsTemplate jmsTemplate;
+
+    @Scheduled(fixedRate = 2000)
+    public void sendMessage(){
+
+        System.out.println("I am sending message");
+        HelloWorldMessage message = HelloWorldMessage
+                .builder()
+                .id(UUID.randomUUID())
+                .message("Hello World")
+                .build();
+
+        jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
+
+        System.out.println("Message send");
+    }
+}
